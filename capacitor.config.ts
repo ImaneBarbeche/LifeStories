f3f8b2c4a1d4306b31f19dc3c0670ef30516433f
GeorgeDaris <george.darisgr@gmail.com>
2025-12-01 14:59:56 +0100

    initial react router and capacitor setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'life-stories',
+  webDir: 'build/client'
+};
+
+export default config;
